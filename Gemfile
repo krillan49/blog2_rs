5708b14829eaac0f105d3c6b6e4d66620ce1306f
krillan <pushkinidze49@gmail.com>
2023-08-28 10:24:34 +0300

    devise установка и настройка

--- Gemfile
+++ Gemfile
@@ -70,3 +70,8 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+
+# ==================== Добавлено ===========================
+
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "home#index"
 
   resources :images do
     resources :comments, only: [:create]
@@ -9,6 +10,4 @@ Rails.application.routes.draw do
   end
 
   resources :comments
-
-  root "home#index"
 end
