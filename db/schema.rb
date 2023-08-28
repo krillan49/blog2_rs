0fa50d95211db500b525382631d9d5f58f84397e
krillan <pushkinidze49@gmail.com>
2023-08-28 10:36:57 +0300

    создана девайс сущность User с дополнительным полем username  и индексом для него

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_22_083150) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_28_072601) do
   create_table "comments", force: :cascade do |t|
     t.string "author"
     t.text "body"
@@ -33,6 +33,20 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_083150) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
