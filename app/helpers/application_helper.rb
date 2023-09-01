fb49e25df337441ec9109ecb7345bf4eb78a0a0a
krillan <pushkinidze49@gmail.com>
2023-09-01 11:49:50 +0300

    условие доступа к изменению контента вынесено в хэлпер

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+  # Для доступа к ссылкам/кнопкам редактирования/удаления контента
+  def have_access_to_content?(object)
+    user_signed_in? && (current_user == object.user or current_user.try(:admin?))
+  end
+  
 end
