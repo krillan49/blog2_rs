33bda8d98d3ed729477891cf4f2acb76ea634905
krillan <pushkinidze49@gmail.com>
2023-09-01 10:25:33 +0300

    add migration with admin column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_30_080618) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_01_072243) do
   create_table "comments", force: :cascade do |t|
     t.string "author"
     t.text "body"
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_080618) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
