5dc4f2daf9b85638adae4ffbf2e1e42653412ae3
krillan <pushkinidze49@gmail.com>
2023-09-02 11:23:40 +0300

    добавлена ссылка на список пользователей в шапке, добавлен хэлпер проверки на админа и метод проверки на админа в контроллере protected, ограничен доступ по юрл и в шапке к списку пользователей для всех кроме админов

--- app/controllers/protected_controller.rb
+++ app/controllers/protected_controller.rb
@@ -1,5 +1,15 @@
 class ProtectedController < ApplicationController
+  before_action :admin?, only: %i[users_list]
+
   def users_list
     @users = User.all
+  end
+
+  private
+
+  def admin?
+    unless user_signed_in? && current_user.try(:admin?)
+      redirect_back fallback_location: root_path, notice: 'User is not admin'
+    end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,5 +4,10 @@ module ApplicationHelper
   def have_access_to_content?(object)
     user_signed_in? && (current_user == object.user or current_user.try(:admin?))
   end
+
+  # Проверка на админа
+  def admin?
+    user_signed_in? && current_user.try(:admin?)
+  end
   
 end
