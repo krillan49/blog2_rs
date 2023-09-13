236ad39c72304d8f2ee83dcc6372bf68bb54f2e9
krillan <pushkinidze49@gmail.com>
2023-09-13 09:36:42 +0300

    настройка маршрутов и метода в ApplicationController для обработки локалей в URL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,25 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  around_action :switch_locale
 
   protected
 
+  # i18n
+  def switch_locale(&action)
+    locale = locale_from_url || I18n.default_locale
+    I18n.with_locale locale, &action
+  end
+
+  def locale_from_url
+    locale = params[:locale]
+    return locale if I18n.available_locales.map(&:to_s).include?(locale)
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  # devise
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,27 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
+
+  scope "(:locale)", locale: /#{I18n.available_locales.join('|')}/ do
+
+    devise_for :users
+    devise_scope :user do
+      get '/users/sign_out' => 'devise/sessions#destroy'
+    end
+    
+    root to: "home#index"
+
+    resources :images do
+      resources :comments, only: [:create]
+    end
+
+    resources :posts do 
+      resources :comments, only: [:create]
+    end
+
+    resources :comments
+
+    get 'users_list' => 'protected#users_list'
+    get 'protected/:id' => 'protected#user_content'
+    
   end
-  
-  root to: "home#index"
 
-  resources :images do
-    resources :comments, only: [:create]
-  end
-
-  resources :posts do 
-    resources :comments, only: [:create]
-  end
-
-  resources :comments
-
-  get 'users_list' => 'protected#users_list'
-  get 'protected/:id' => 'protected#user_content'
 end
