0b5f0015c8a4a9105fd7cd466085bdeca10ac2b8
krillan <pushkinidze49@gmail.com>
2023-09-15 11:18:16 +0300

    тесты для модели image

---
+++ spec/models/image_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Image do
+
+  describe "validations" do
+    it { should validate_presence_of :url }
+  end
+
+  describe "assotiations" do
+    # it { should belong_to :user }
+    it { should have_many :comments}
+  end
+
+end
