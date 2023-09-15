5b83a62ba07f7076717cc2f6e035b8dec4a55dcd
krillan <pushkinidze49@gmail.com>
2023-09-15 11:29:02 +0300

    тесты для модели post

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Post do
+
+  describe "validations" do
+    it { should validate_presence_of :content }
+    it { should validate_length_of(:content).is_at_least(100).is_at_most(5000) }
+  end
+
+  describe "assotiations" do
+    # it { should belong_to :user }
+    it { should have_many :comments}
+  end
+
+end
