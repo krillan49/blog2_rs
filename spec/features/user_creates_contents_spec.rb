eb8a1c7ad962db057be6dc550ce882aa89220b81
krillan <pushkinidze49@gmail.com>
2023-09-19 11:38:05 +0300

    подключение доступа супорт фаилов в рэилс_хэлпер, тест регистрации нового юзера и хэлпердля того же

---
+++ spec/features/user_creates_contents_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+feature "Create contents" do
+
+  # scenario "access to home#index/root page" do
+  #   visit root_path
+  #   expect(page).to have_content I18n.t('home.index.title')
+  # end
+
+end
